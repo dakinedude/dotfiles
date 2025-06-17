4435d7c3c160f60c5c29a0a677e15334253aff68
dakinedude <andersenmats@pm.me>
2025-06-17 02:03:30 +0200

    fix

--- .config/emacs/lisp/ui.el
+++ .config/emacs/lisp/ui.el
@@ -1,11 +1,8 @@
-;;; ui.el --- UI settings -*- lexical-binding: t; -*-
-
 (set-face-attribute 'default nil :family "Source Code Pro" :height 140)
 
-(when (display-graphic-p)
-  (use-package gruber-darker-theme
-    :demand t
-    :config
-    (load-theme 'gruber-darker t)))
+(use-package gruber-darker-theme
+  :demand t
+  :config
+  (load-theme 'gruber-darker t))
 
 (provide 'ui)
